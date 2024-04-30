06eba39e5f61c733db4a8ae2786b1351959f2989
Gayloc <gu18647403665@outlook.com>
2024-04-30 16:16:13 +0800

    feat: 新增文件类型筛选

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileWriter;
@@ -31,22 +32,40 @@ public class MainWindow {
     private static JMenu getFileMenu(JFrame frame) {
         JMenu fileMenu = new JMenu("文件");
 
+        JMenuItem itemSave = new JMenuItem(new AbstractAction("保存") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                c.save();
+            }
+        });
+        itemSave.setEnabled(false);
+
         JMenuItem itemNew = new JMenuItem(new AbstractAction("新建…") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileChooser = new JFileChooser(".");
+
+                fileChooser.setFileFilter(new xmlFileFilter());
+
                 fileChooser.showSaveDialog(frame);
                 File file = fileChooser.getSelectedFile();
 
-                try {
-                    FileWriter writer = new FileWriter(file);
-                    writer.write("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?><Students/>");
-                    writer.close();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                if(file != null) {
+                    try {
+                        FileWriter writer = new FileWriter(file);
+                        writer.write("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?><Students/>");
+                        writer.close();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
 
-                c=new Controller(file);
+                    if (file.getName().endsWith(".xml")) {
+                        c = new Controller(file);
+                        itemSave.setEnabled(true);
+                    } else {
+                        JOptionPane.showMessageDialog(frame,"文件格式不正确", "错误", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
             }
         });
 
@@ -54,13 +73,47 @@ public class MainWindow {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileChooser = new JFileChooser(".");
+
+                fileChooser.setFileFilter(new xmlFileFilter());
+
                 fileChooser.showOpenDialog(frame);
                 File file = fileChooser.getSelectedFile();
-                c=new Controller(file);
+
+                if(file!=null) {
+                    if (file.getName().endsWith(".xml")) {
+                        c = new Controller(file);
+                        itemSave.setEnabled(true);
+                    } else {
+                        JOptionPane.showMessageDialog(frame,"文件格式不正确", "错误", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
             }
         });
+
+        JMenuItem itemExit = new JMenuItem(new AbstractAction("退出") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
         fileMenu.add(itemNew);
         fileMenu.add(itemOpen);
+        fileMenu.add(itemSave);
+        fileMenu.add(itemExit);
         return fileMenu;
+    }
+}
+
+class xmlFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+        if (f.isDirectory())return true;
+        return f.getName().endsWith(".xml");
+    }
+
+    @Override
+    public String getDescription() {
+        return ".xml";
     }
 }
